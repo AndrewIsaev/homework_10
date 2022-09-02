e47b1d71f48a523f800ad5f234f9c10e6bb7608c
andrey.isaev <an.isaev7@gmail.com>
2022-09-02 11:42:21 +0300

    feat: create get_all() function

--- utils.py
+++ utils.py
@@ -7,12 +7,20 @@ def load_candidates(candidates):
     with open(candidates, encoding="utf-8") as file:
         return json.load(file)
 
+
 def get_all():
-    pass
+    candidate_list = []
+    for candidate in load_candidates(CANDIDATES):
+        candidate_output = "\n".join(
+            [candidate["name"], candidate["position"], candidate["skills"]])
+        candidate_list.append(candidate_output)
+    return '\n\n'.join(candidate_list)
 
 def get_by_pk(pk):
     pass
 
+
 def get_by_skill(skill_name):
     pass
 
+print(get_all())
