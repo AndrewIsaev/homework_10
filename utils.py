549d0650d9f8a00f827dbba086e7676b84061080
andrey.isaev <an.isaev7@gmail.com>
2022-09-02 12:13:37 +0300

    feat: create get_by_skills function

--- utils.py
+++ utils.py
@@ -21,8 +21,17 @@ def get_by_pk(pk):
     for candidate in load_candidates(CANDIDATES):
         if candidate["pk"] == pk:
             url = candidate["picture"]
-            return '\n'.join([url, candidate["name"], candidate["position"], candidate["skills"]])
+            return '\n'.join([url, candidate["name"], candidate["position"],
+                              candidate["skills"]])
 
 
-def get_by_skill(skill_name):
-    pass
+def get_by_skill(skill_name: str):
+    candidate_by_skills = []
+    for candidate in load_candidates(CANDIDATES):
+        if  skill_name.lower() in candidate["skills"].lower().split(", "):
+            candidate_output = "\n".join(
+                [candidate["name"], candidate["position"],
+                 candidate["skills"]])
+            candidate_by_skills.append(candidate_output)
+    return "\n\n".join(candidate_by_skills)
+
